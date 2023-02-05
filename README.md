402ca3a5c2e09bb0d35d2e38c0a6c738c680e4c4
foggy21 <rashit.xaliullin@inbox.ru>
2023-02-05 13:26:05 +1000

    Add quick sort and update readme

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# librarySearch
+# Library Search
+<b><h3>Проект для повторения и закрепления эффективных алгоритмов.</h3></b>
+Приложение для выбора и добавления своих книг.</br>
+***
+Задачи:</br>
+---
+1.  Стелаж состоит из полок, на каждой полке помещается одинаковое количество книг. На полках могут быть пустые места.
+2.  Каждая полка должна хранить упорядоченный набор книг по фамилии автора
+3.  Пользователь может найти книгу по автору, а также по позиции на стелаже.
+4.  Пользователь может добавить свою книгу на стелаж, если имеется пустое место на полках. Можно добавлять книгу только в соответствии со следующим форматом: **И.О.Фамилия**
+---
+## Алгоритмы
+</br>
+<p style="font-size: 14px">Для сортировки книг на полках был использован эффективный алгоритм быстрой сортировки.</p>
+
+```cs
+static void quickSort(string[] books, int left, int right)
+{
+    if (left >= right) return;
+    int baseElement = books[(left + right) / 2][4]; // Choose first letter in surname.
+    int i = left;
+    int j = right;
+    while (i <= j)
+    {
+        while (books[i][4] < baseElement) i++;
+        while (books[j][4] > baseElement) j--;
+        if (i <= j)
+        {
+            string tempBook = books[i];
+            books[i] = books[j];
+            books[j] = tempBook;
+            i++;
+            j--;
+        }
+    }
+    quickSort(books, left, j);
+    quickSort(books, i, right);
+}
+```
+<p style="font-size: 16px">Недостаток конкретно этой сортировки в том, что нет сравнения по следующим буквам фамилии, если первые буквы одинаковые</p>
+
+><em style="font-size: 15px">Запишем это в список доработок</em>
+</br>
+
+---
+
+## Список доработок
+<p style="font-size:14px">
+1. Грамотно решить проблему сортировки книг, сравнивая следующие буквы, если первые буквы фамилии автора одинаковые.
+</br>
+</p>
+
+---
+# Спойлер
+<p style="font-size:20px"> Этот проект является учебным для улучшения навыков описания репозитория и ведения документации проекта. Идея взята с <a href="https://www.youtube.com/watch?v=w8rRhAup4kg">курса</a> Романа Сакутина и немного модифицирована.</p>
+
+---
+# Spoiler
+<p style="font-size:20px"> This study project is designed to upgrade skills in the description repository and record management of project. The idea of the project borrowed from Roman Sakutin's <a href="https://www.youtube.com/watch?v=w8rRhAup4kg">course</a></p>
+
--- librarySearch/Program.cs
+++ librarySearch/Program.cs
@@ -8,8 +8,78 @@ namespace librarySearch
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            string[,] books = new string[5, 4] {
+                { "А.П.Чехов", "А.С.Пушкин", "Ф.М.Достоевский", "C.А.Есенин" },
+                { "М.Ю.Лермонтов", "И.С.Тургенев", "Н.В.Гоголь", "Н.А.Некрасов" },
+                { "В.В.Маяковский", "И.А.Бунин", "Б.Л.Постернак", "А.И.Куприн" }, 
+                { "Ф.И.Тютчев", "В.А.Жуковский", "И.А.Крылов", "М.В.Ломоносов" }, 
+                { "Пустая полка", "Пустая полка", "Пустая полка", "Пустая полка" },
+            };
+            const int booksInRow = 4;
+
+            bool isOpen = true;
+            int rowIterator = 0;
+            int columnIterator = 0;
+            string userInput;
+
+            string[] rowOfBooks = new string[booksInRow];
+            foreach (string book in books)
+            {
+                rowOfBooks[columnIterator++] = book;
+                if (columnIterator >= booksInRow)
+                {
+                    quickSort(rowOfBooks, 0, booksInRow - 1);
+                    for (int i = 0; i < booksInRow; i++)
+                    {
+                        books[rowIterator, i] = rowOfBooks[i];
+                    }
+                    rowIterator++;
+                    columnIterator = 0;
+                }
+            }
+
+            while(isOpen) { 
+                Console.SetCursorPosition(0, 21);
+                Console.Write("Стелаж с книгами:\n");
+                foreach(string book in books)
+                {
+                    Console.Write($"{book}, ");
+                    if ( ++columnIterator % booksInRow == 0 ) Console.WriteLine();
+                }
+
+
+                columnIterator = 0;
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Библиотека\n\nВведите команду:\n\n1 - Поиск книги по автору\n\n2 - Поиск книги по стелажу\n\n3 - Добавление книги\n\nВвод команды:");
+                //quickSort(books, 0, books.Length - 1);
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        static void quickSort(string[] books, int left, int right)
+        {
+            if (left >= right) return;
+            int baseElement = books[(left + right) / 2][4]; // Choose first letter in surname.
+            int i = left;
+            int j = right;
+            while (i <= j)
+            {
+                while (books[i][4] < baseElement) i++;
+                while (books[j][4] > baseElement) j--;
+                if (i <= j)
+                {
+                    string tempBook = books[i];
+                    books[i] = books[j];
+                    books[j] = tempBook;
+                    i++;
+                    j--;
+                }
+            }
+            quickSort(books, left, j);
+            quickSort(books, i, right);
         }
     }
 }
